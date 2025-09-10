1713d456d2a4d5ca27cc42b431d667573dcf4d14
Nicholas Farraj <farrajnj@udel.edu>
2025-09-10 11:03:18 -0400

    did a couple functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,12 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    } else if (numbers.length === 1) {
+        return [numbers[0], numbers[0]];
+    }
+    return [numbers[0], numbers[numbers.length - 1]];
 }
 
 /**
@@ -13,7 +18,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((num) => num * 3);
+    return tripled;
 }
 
 /**
@@ -21,7 +27,8 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const converted = numbers.map((str) => parseInt(str) || 0);
+    return converted;
 }
 
 /**
@@ -32,7 +39,11 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    const noDollar = amounts.filter((str: string) =>
+        str.startsWith("$") ? str.slice(1) : str,
+    );
+    const converted = noDollar.map((str: string) => parseInt(str) || 0);
+    return converted;
 };
 
 /**
