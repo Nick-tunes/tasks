ab646f2eefc63d62708ed194c17280ce5d0aeb4a
Nicholas Farraj <farrajnj@udel.edu>
2025-09-15 20:38:55 -0400

    finished all functions but ran into problems with test cases

--- src/objects.ts
+++ src/objects.ts
@@ -80,7 +80,11 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        return `# ${question.name}\n${question.body}\n- ${question.options.join().replaceAll(",", "\n- ")}`;
+    } else {
+        return `# ${question.name}\n${question.body}`;
+    }
 }
 
 /**
@@ -88,6 +92,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
+    question.name = newName;
     return question;
 }
 
@@ -97,7 +102,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQ: Question = { ...question };
+    newQ.published = !newQ.published;
+    return newQ;
 }
 
 /**
@@ -107,6 +114,8 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
+    const newName = `Copy of ${oldQuestion.name}`;
+    oldQuestion = { ...oldQuestion, id: id, name: newName, published: false };
     return oldQuestion;
 }
 
@@ -118,6 +127,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
+    question.options = [...question.options, newOption];
     return question;
 }
 
@@ -135,5 +145,12 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    const newQ: Question = {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false,
+    };
+    return newQ;
 }
